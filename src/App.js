026ef358219984e4fde18f0bf4d086fa290fbb9c
jhw <jjsu4852@naver.com>
2022-08-23 14:25:28 +0900

    add fog

--- src/App.js
+++ src/App.js
@@ -1,53 +1,58 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react"
 function App() {
-  const API_KEY = process.env.REACT_APP_API_KEY;
+  const apiKey = process.env.REACT_APP_API_KEY;
   const [query,Setquery] = useState('');
   const [weather,Setweather] = useState('');
-  const onSubmit = e => e.preventDefault();
-  
-  const today = () => {
+  const baseUrl = 'https://api.openweathermap.org/data/2.5/'
+  const today = (a) => {
     let date = new Date()
     let Year = date.getFullYear()
-    let month = (date.getMonth()+1) // 0부터 시작하므로 +1을 해준다.
-    let day = date.getDate()
+    let month = (date.getMonth()+1)
+    let day = (date.getDate()+a)
     return `${Year} . ${month} . ${day}` 
   }
-  console.log(today(new Date()))
-  console.log(today())
-  useEffect(()=>{const  search = async e =>{
+  const weatherData = async (e) =>{
     if(e.key === 'Enter'){
-     await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
-      .then( result => { Setweather(result);})
+    await  axios.get(`${baseUrl}weather?q=${query}&units=metric&appid=${apiKey}`)
+      .then( result => { Setweather(result); })
       .catch(()=>{alert('Please Enter Detail ex)london, busan...');})
       Setquery('')
     }
-  }},[])
+ }
+useEffect(() => { axios.get(`${baseUrl}weather?q=seoul&units=metric&appid=${apiKey}`)
+.then( (res) => { Setweather(res)})
+  },[apiKey])
+    
    
   return (
-    <div className={`App ${weather.data?.weather[0].main == 'Clouds' ? 'bg-cloud' 
-    : weather.data?.weather[0].main == 'Rain' ? 'bg-rain'
-    : weather.data?.weather[0].main == 'Drizzle' ? 'bg-rain'
-    : weather.data?.weather[0].main == 'Clear' ? 'bg-sunny' 
-    : 'bg-basic'}`}>
+    <div className={
+    `App ${weather.data?.weather[0].main === 'Clouds' ? 'bg-cloud' 
+    : weather.data?.weather[0].main === 'Rain' ? 'bg-rain'
+    : weather.data?.weather[0].main === 'Mist' ? 'bg-fog'
+    : weather.data?.weather[0].main === 'Drizzle' ? 'bg-rain'
+    : weather.data?.weather[0].main === 'Clear' ? 'bg-sunny' 
+    : 'bg-basic'}`
+    }>
      <main>
-       <div className="searchBox">
-         <input type="text" value={query} onChange={e => Setquery(e.target.value)} className="searchBar" onKeyPress={search} placeholder="ex)seoul"/>
+     <div className="searchBox">
+         <input type="text" value={query} onChange={e => Setquery(e.target.value)} className="searchBar" onKeyPress={weatherData} placeholder="ex)busan, london..."/>
           </div>
+          <div className="informaitonBox">
           {weather.data ?
-          <div>
+          <>
             <div className="locationBox">
               <div className="location">{weather.data.name}, {weather.data.sys.country}</div>
-              <div className="date">{today()}</div>
+              <div className="date">{today(0)}</div>
             </div>
             <div className="weatherBox">
               <div className="temp">{weather.data.main.temp}℃</div>
-              <div className="weather">weather : {weather.data.weather[0].main}</div>
               <div className="humidity">humidity : {weather.data.main.humidity} %</div>
               <div className="wind">wind : {weather.data.wind.speed} m/s, {weather.data.wind.deg} deg</div>
               {weather.data.rain ? <div className="rain">rain : {weather.data.rain['1h']} mm</div> : null}
         </div>
-      </div> : null}
+      </> : null}
+       </div>
      </main>
     </div>
   );
